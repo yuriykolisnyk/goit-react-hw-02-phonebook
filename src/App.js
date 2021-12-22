ca0ea893a8ff7ef6aa4017fb66d3d343b8412b0e
yuriykolisnyk <ykolisnyk@gmail.com>
2021-12-22 15:52:36 +0200

    step 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return <div className="App">TEST</div>;
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  render() {
+    return (
+      <>
+        <div>
+          <h1>Phonebook</h1>
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+          <input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+          <button type="button">Add contact</button>
+          <h2>Contacts</h2>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
